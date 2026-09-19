23bb271ddf62a7015f25e35aba2d34cb51f87ac0
anushkumar701 <anushk0908@gmail.com>
2026-09-19 13:54:01 +0530

    chore: bump version to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.fruitbilling.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.2.0"
+        versionCode = 3
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
